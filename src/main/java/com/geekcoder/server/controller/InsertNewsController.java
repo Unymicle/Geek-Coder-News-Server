793d38e5b1fb516135a976d8c6a61b12f03d3281
ChengLiufeng <liufeng.cheng@ericsson.com>
2014-11-01 15:58:42 +0800

    create controller for news and category

---
+++ src/main/java/com/geekcoder/server/controller/InsertNewsController.java
@@ -0,0 +1,15 @@
+package com.geekcoder.server.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class InsertNewsController implements Controller {
+
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
