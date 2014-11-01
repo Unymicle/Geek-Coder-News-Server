c09ced989e2545772388229abf2b0c0be72ebb3f
ChengLiufeng <liufeng.cheng@ericsson.com>
2014-11-01 23:03:22 +0800

    finish InsertNewsController interface

--- src/main/java/com/geekcoder/server/controller/InsertNewsController.java
+++ src/main/java/com/geekcoder/server/controller/InsertNewsController.java
@@ -2,14 +2,38 @@ package com.geekcoder.server.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import com.geekcoder.server.dao.DAOFactory;
+import com.geekcoder.server.model.News;
 
 public class InsertNewsController implements Controller {
 
-	public ModelAndView handleRequest(HttpServletRequest arg0,
-			HttpServletResponse arg1) throws Exception {
-		// TODO Auto-generated method stub
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		int cid = Integer.parseInt(request.getParameter("cid"));
+		String title = request.getParameter("title");
+		String keywords = request.getParameter("keywords");
+		String description = request.getParameter("description");
+		int status = Integer.parseInt(request.getParameter("status"));
+		String summary = request.getParameter("summary");
+		int published = Integer.parseInt(request.getParameter("published"));
+		int updateTime = Integer.parseInt(request.getParameter("updateTime"));
+		String content = request.getParameter("content");
+		int aid = Integer.parseInt(request.getParameter("aid"));
+		News news = new News();
+		news.setCid(cid);
+		news.setTitle(title);
+		news.setKeywords(keywords);
+		news.setDescription(description);
+		news.setStatus(status);
+		news.setSummary(summary);
+		news.setPublished(published);
+		news.setUpdateTime(updateTime);
+		news.setContent(content);
+		news.setAid(aid);
+		DAOFactory.getNewsDAOInstance().insert(news);
 		return null;
 	}
 }
